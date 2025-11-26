e38952fba799959ac360d28b16498320964884c6
Christian McLaughlin <cmclaughlin@redteamcafe.com>
2025-11-26 21:19:29 +0000

    Update file autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -7,22 +7,23 @@ REPO_NAME="kvmage"
 SCRIPTS_DIR="scripts"
 
 echo "[*] Cloning repository..."
-git clone "$REPO_URL"
+rm -rf "${REPO_NAME}"
+git clone "${REPO_URL}"
 
 echo "[*] Entering repo directory..."
-cd "$REPO_ROOT"
+cd "${REPO_ROOT}"
 
 echo "[*] Creating dist directory..."
 mkdir -p dist
 
 echo "[*] Running build.sh..."
-bash $SCRIPTS_DIR/kvmage-build.sh
+bash "${SCRIPTS_DIR}/kvmage-build.sh"
 
 echo "[*] Running install.sh..."
-bash $SCRIPTS_DIR/kvmage-install.sh
+bash "${SCRIPTS_DIR}/kvmage-install.sh"
 
 echo "[*] Cleaning up..."
 cd ..
-rm -rf "$REPO_ROOT"
+rm -rf "${REPO_ROOT}"
 
 echo "[*] Done."
