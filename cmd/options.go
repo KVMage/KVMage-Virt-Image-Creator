4f526ac96fcd7e6dc5918cab2c9eb4f69ec2c105
Christian McLaughlin <cmclaughlin@redteamcafe.com>
2026-01-26 02:39:46 +0000

    Update file options.go

--- cmd/options.go
+++ cmd/options.go
@@ -8,22 +8,22 @@ import (
 )
 
 type Options struct {
-	Profile          string   `yaml:"-"`                         // Internal use only, not from YAML
-	VirtMode         string   `yaml:"virt_mode"`                 // "install" or "customize"
-	ImageName        string   `yaml:"image_name"`                // Name of the image
-	OSVariant        string   `yaml:"os_var"`                    // OS variant (from osinfo-query)
-	ImageSize        string   `yaml:"image_size"`                // e.g., "100G"
-	ImagePartition   string   `yaml:"image_part"`                // e.g. "/dev/sda1"
-	InstallFile      string   `yaml:"install_file" file:"true"`  // Path to auto install file
-	InstallMedia     string   `yaml:"install_media" file:"true"` // Local or remote ISO file or install tree
-	ImageSource      string   `yaml:"image_src" file:"true"`     // Source QCOW2 image (customize mode)
-	ImageDestination string   `yaml:"image_dest" file:"true"`    // Output QCOW2 image
-	Upload			 []string `yaml:"upload" file:"true"`		 // Files or directories to upload (temp)
-	Execute			 []string `yaml:"execute": file:"true"`		 // Files to execute scripts (in order)
-	Hostname         string   `yaml:"hostname"`                  // Optional
-	Network          string   `yaml:"network"`                   // Optional virtual network
-	Console          string   `yaml:"console"`                   // "serial", or "graphical"
-	Firmware         string   `yaml:"firmware"`                  // "bios" or "efi" (default: bios)
+	Profile          	string   	`yaml:"-"`                         	// Internal use only, not from YAML
+	VirtMode         	string   	`yaml:"virt_mode"`                 	// "install" or "customize"
+	ImageName        	string   	`yaml:"image_name"`                	// Name of the image
+	OSVariant        	string   	`yaml:"os_var"`                    	// OS variant (from osinfo-query)
+	ImageSize        	string   	`yaml:"image_size"`                	// e.g., "100G"
+	ImagePartition   	string   	`yaml:"image_part"`                	// e.g. "/dev/sda1"
+	InstallFile      	string   	`yaml:"install_file" file:"true"`  	// Path to auto install file
+	InstallMedia     	string   	`yaml:"install_media" file:"true"` 	// Local or remote ISO file or install tree
+	ImageSource      	string   	`yaml:"image_src" file:"true"`     	// Source QCOW2 image (customize mode)
+	ImageDestination 	string   	`yaml:"image_dest" file:"true"`    	// Output QCOW2 image
+	Upload				[]string 	`yaml:"upload" file:"true"`		 	// Files or directories to upload (temp)
+	Execute			 	[]string 	`yaml:"execute": file:"true"`		// Files to execute scripts (in order)
+	Hostname         	string   	`yaml:"hostname"`                  	// Optional
+	Network          	string   	`yaml:"network"`                   	// Optional virtual network
+	Console          	string		`yaml:"console"`                   	// "serial", or "graphical"
+	Firmware         	string		`yaml:"firmware"`                  	// "bios" or "efi" (default: bios)
 }
 
 func resolvePath(path string) string {
